cd44aef6fe0a4428250b12f899fe7cc7121cb71d
Hails <stuart.vonn@gmail.com>
2016-01-23 05:58:56 -0200

    Get Trot working

--- lib/moyashi.ex
+++ lib/moyashi.ex
@@ -1,2 +1,12 @@
 defmodule Moyashi do
 end
+
+defmodule Moyashi.Router do
+  use Trot.Router
+
+  get "/" do
+    {:ok, %{hello: "world"}, %{"Content-Type" => "application/json"}}
+  end
+
+  import_routes Trot.NotFound
+end
