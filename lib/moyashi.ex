f69ec342c309f5230da5ad811bd22a5517dd8644
Hails <stuart.vonn@gmail.com>
2016-01-23 05:15:19 -0200

    Fuck maru and its shitty documentation

--- lib/moyashi.ex
+++ lib/moyashi.ex
@@ -1,2 +1,33 @@
 defmodule Moyashi do
 end
+
+defmodule Moyashi.Router.Home do
+  use Maru.Router
+
+  get do
+    conn
+    |> json(%{hello: :world})
+  end
+
+end
+
+defmodule Moyashi.Router.Board do
+end
+
+  namespace :boards do
+    params do
+
+    end
+end
+
+defmodule Moyashi.API do
+  use Maru.Router
+
+  mount Moyashi.Router.Home
+
+  rescue_from :all do
+    conn
+    |> put_status(500)
+    |> text("Server Error")
+  end
+end
