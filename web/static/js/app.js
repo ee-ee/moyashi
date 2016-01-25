88d2fb05573c596c5765a036a03f3862ee720560
ee-ee <eeee@openmailbox.org>
2016-01-25 18:29:56 -0200

    Basic inline expansion.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,48 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
-import "phoenix_html";
+'use strict';
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
+var ready = function(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+};
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+var App = {
+    inlineExpansion: function() {
+        var elements = document.querySelectorAll('a.image');
+        Array.prototype.forEach.call(elements, function(el, i) {
+            el.addEventListener('click', App.expandImage);
+        });
+    },
+    expandImage: function(e) {
+        // check for middle click
+        if(e.which == 2) {
+            return;
+        }
+        e.preventDefault();
+        var thumb = this.querySelectorAll('img.thumb')[0];
+        thumb.classList.add('hidden');
+        var fullimage = document.createElement('img');
+        fullimage.src = this.href;
+        fullimage.classList.add('full-image');
+        this.appendChild(fullimage);
+        this.removeEventListener('click', App.expandImage);
+        this.addEventListener('click', App.contractImage);
+    },
+    contractImage: function(e) {
+        // check for middle click
+        if(e.which == 2) {
+            return;
+        }
+        e.preventDefault();
+        var thumb = this.querySelectorAll('img.thumb')[0];
+        thumb.classList.remove('hidden');
+        var fullimage = this.querySelectorAll('img.full-image')[0];
+        this.removeChild(fullimage);
+        this.removeEventListener('click', App.contractImage);
+        this.addEventListener('click', App.expandImage);
+    }
+};
 
-//import socket from "./socket"
+ready(App.inlineExpansion);
