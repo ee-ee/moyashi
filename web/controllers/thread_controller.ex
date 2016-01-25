99751ba374787c80840a8a8b67fd367190f64ddb
ee-ee <eeee@openmailbox.org>
2016-01-25 00:33:31 -0200

    Threads index.

--- web/controllers/thread_controller.ex
+++ web/controllers/thread_controller.ex
@@ -2,13 +2,16 @@ defmodule Moyashi.ThreadController do
   use Moyashi.Web, :controller
 
   alias Moyashi.Thread
+  alias Moyashi.Board
 
   def index(conn, %{"board_id" => board_id}) do
+    boards = Repo.all(Board)
     query = from t in Thread,
           where: t.board_id == ^board_id,
          select: t
     threads = Repo.all(query)
+    board = Repo.get!(Board, board_id)
 
-    render(conn, "index.html", threads: threads)
+    render(conn, "index.html", threads: threads, board: board, boards: boards)
   end
 end
