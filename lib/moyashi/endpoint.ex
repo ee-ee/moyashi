9ef01b2343a060ed6fbab2b4307bead0d6c88799
ee-ee <eeee@openmailbox.org>
2016-02-02 13:28:23 -0200

    Fixes the route for serving uploaded images.

--- lib/moyashi/endpoint.ex
+++ lib/moyashi/endpoint.ex
@@ -11,6 +11,9 @@ defmodule Moyashi.Endpoint do
     at: "/", from: :moyashi, gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
+  plug Plug.Static,
+    at: "/files", from: "files", gzip: true
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
