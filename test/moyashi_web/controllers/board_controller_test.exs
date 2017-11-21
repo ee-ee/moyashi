1c5e012617827085e16266d2c3993ae67fafb00c
Allan J <contato.allanj@gmail.com>
2017-11-20 23:07:02 -0200

    tests: refactor json rendering of board controller test

--- test/moyashi_web/controllers/board_controller_test.exs
+++ test/moyashi_web/controllers/board_controller_test.exs
@@ -1,17 +1,20 @@
 defmodule MoyashiWeb.BoardControllerTest do
-    use MoyashiWeb.ConnCase
+  use MoyashiWeb.ConnCase
 
-    test "#index renders a list of boards" do
-        conn = build_conn()
-        board = insert(:board)
+  test "#index renders a list of boards" do
+    conn = build_conn()
+    boards = Enum.map([1..10], fn -> insert(:board) end)
 
-        conn = get conn, board_path(conn, :index)
+    conn = get conn, board_path(conn, :index)
 
-        assert json_response(conn, 200) == [
-            %{
-                "title" => board.title,
-                "slug" => board.slug
-            }
-        ]
-    end
+    assert json_response(conn, 200) == render_json("index.json", boards: boards)
+  end
+
+  defp render_json(template, assigns) do
+    assigns = Map.new(assigns)
+
+    MoyashiWeb.API.BoardView.render(template, assigns)
+    |> Poison.encode!
+    |> Poison.decode!
+  end
 end
