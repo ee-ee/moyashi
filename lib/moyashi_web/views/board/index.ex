fda17e2c0ee2c8f5f9fc4cc4297a136fe731e966
Allan J <contato.allanj@gmail.com>
2017-11-21 00:49:41 -0200

    view: refactor board render function

--- lib/moyashi_web/views/board/index.ex
+++ lib/moyashi_web/views/board/index.ex
@@ -1,14 +1,13 @@
 defmodule MoyashiWeb.API.BoardView do
     use MoyashiWeb, :view
+    alias MoyashiWeb.API.BoardView
 
     def render("index.json", %{boards: boards}) do
-        Enum.map(boards, &board_json/1)
+        #Enum.map(boards, &board_json/1)
+      render_many(boards, BoardView, "board.json")
     end
 
-    defp board_json(board) do
-        %{
-            title: board.title,
-            slug: board.slug
-        }
+    def render("board.json", %{board: board}) do
+      %{title: board.title, slug: board.slug}
     end
 end
